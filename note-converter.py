65c4144dcf1e73906576f35edfebda1a5d972c57
alpaylan <alpkeles99@gmail.com>
2023-02-15 21:49:24 -0500

    fix naming issue and recompile

--- note-converter.py
+++ note-converter.py
@@ -4,9 +4,9 @@ import os
 
 if __name__ == "__main__" :
     for i in range(1, 30):
-        print(f"Converting {i:02d}.02.23.md to {i}.02.23.pdf")
+        print(f"Converting {i:02d}.02.23.md to {i:02d}.02.23.pdf")
         try:
-            os.system(f"pandoc {i:02d}.02.23.md -o {i}.02.23.pdf")
+            os.system(f"pandoc {i:02d}.02.23.md -o {i:02d}.02.23.pdf")
             print("Done!")
         except Exception as e:
             print(f"Conversion failed: {e}")
