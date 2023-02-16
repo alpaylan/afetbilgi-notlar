2de8ba8be8db3caaa7eb7b2c9d6a0f99a76f7bf4
alpaylan <alpkeles99@gmail.com>
2023-02-15 23:13:59 -0500

    update converter to make it smarter

--- note-converter.py
+++ note-converter.py
@@ -3,10 +3,18 @@
 import os
 
 if __name__ == "__main__" :
-    for i in range(1, 30):
-        print(f"Converting {i:02d}.02.23.md to {i:02d}.02.23.pdf")
+    files = os.listdir()
+    notes = [file for file in files if (file.endswith(".md") and not file.startswith("README"))]
+    # get all changed files from git
+    changed = os.popen("git diff --name-only").read().splitlines()
+    # filter out files that are not changed
+    converted_files = [file for file in notes if file in changed]
+
+
+    for file in converted_files:
+        print(f"Converting {file} to pdf...")
         try:
-            os.system(f"pandoc {i:02d}.02.23.md -o {i:02d}.02.23.pdf")
+            os.system(f'pandoc {file} -o {file.split(".md")[0]}.pdf')
             print("Done!")
         except Exception as e:
             print(f"Conversion failed: {e}")
