75591cbbbe61deb6cb63feaafbf858ddb12d4ab4
alpaylan <alpkeles99@gmail.com>
2023-02-17 03:52:57 -0500

    configure autoupdate readme

--- README.md
+++ README.md
@@ -3,3 +3,13 @@
 [tr] AfetBilgi nin geliştirilme sürecinde aldığım notları burada paylaşacağım. Sürecin düzenli ve dürüst bir anlatımı olması açısından, ileride geriye baktığımızda yaptığımız hataları görebilmemiz, neleri düşünmüştük de unuttuk acaba diye araştırmamız açısından iyi olacağını düşünüyorum.
 
 [en] I'll share the notes I took during the development of AfetBilgi here. I think it will be good for us to be able to see the mistakes we made in the process, to be able to research what we thought of but forgot later, also to have a regular and honest explanation of the process.
+
+## Notlar
+
+- [05.02.23](./05.02.23.md)
+- [06.02.23](./06.02.23.md)
+- [10.02.23](./10.02.23.md)
+- [11.02.23](./11.02.23.md)
+- [12.02.23](./12.02.23.md)
+- [13.02.23](./13.02.23.md)
+- [15.02.23](./15.02.23.md)
--- note-converter.py
+++ note-converter.py
@@ -11,6 +11,7 @@ if __name__ == "__main__" :
     converted_files = [file for file in notes if file in changed]
 
 
+    # Update files
     for file in converted_files:
         print(f"Converting {file} to pdf...")
         try:
@@ -18,3 +19,19 @@ if __name__ == "__main__" :
             print("Done!")
         except Exception as e:
             print(f"Conversion failed: {e}")
+
+
+    # Update README.md
+    print("Updating README.md...")
+    readme = open("README.md", "w")
+    readme.write("# AfetBilgi Notlar\n")
+    readme.write("""
+[tr] AfetBilgi nin geliştirilme sürecinde aldığım notları burada paylaşacağım. Sürecin düzenli ve dürüst bir anlatımı olması açısından, ileride geriye baktığımızda yaptığımız hataları görebilmemiz, neleri düşünmüştük de unuttuk acaba diye araştırmamız açısından iyi olacağını düşünüyorum.
+
+[en] I'll share the notes I took during the development of AfetBilgi here. I think it will be good for us to be able to see the mistakes we made in the process, to be able to research what we thought of but forgot later, also to have a regular and honest explanation of the process.\n""")
+    readme.write("\n## Notlar\n")
+    for file in sorted(notes):
+        readme.write(f"\n- [{file.split('.md')[0]}](./{file})")
+
+    readme.write("\n")
+    readme.close()
